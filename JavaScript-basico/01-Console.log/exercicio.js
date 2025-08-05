c840f5b4d871a41fe7a9c3a9328aa892803de508
João Rodrigo <joaofreire185710@gmail.com>
2025-08-05 14:33:05 -0300

    first commit

---
+++ JavaScript-basico/01-Console.log/exercicio.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "João". Estou aprendendo JavaScript as 14 da tarde')
---
+++ JavaScript-basico/01-Console.log/script.js
@@ -0,0 +1,5 @@
+console.log("Hello 'World'"); // Quando for envolver uma palavra com aspas simples
+console.log('Hello "World"'); // Qunado for envolver uma palavra com aspas duplas
+console.log(`'Hello' "World"`); // Conceito mais avançado, mas nele pode ser usado aspas duplas e simples
+
+console.log(12, 15, 17.89, "Hello World"); // No caso de números eu coloco os números dentro dos parênteses sem usar as aspas. Eu também posso colocar vários valores, separando eles por vírgula
