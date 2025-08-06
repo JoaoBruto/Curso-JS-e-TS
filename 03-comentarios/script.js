59e30dd1b02fea1f11fb44b9199564cc3ba186e9
João Rodrigo <joaofreire185710@gmail.com>
2025-08-06 18:30:50 -0300

    📝 comments in the code

---
+++ 03-comentarios/script.js
@@ -0,0 +1,9 @@
+// Comentário
+// Ele não será interpretado e nem aparecerá no site (terminal)
+// Comentário de linha
+/*
+Comentário de bloco
+*/
+console.log("Olá mundo")
+console.log("Olá mundo")
+console.log("Olá mundo")
