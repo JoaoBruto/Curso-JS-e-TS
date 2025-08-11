08d661489e7be42fcde88e7527c7069f3b43e53f
João Rodrigo <joaofreire185710@gmail.com>
2025-08-11 17:33:40 -0300

    📝 Rules-Const-And-Examples

--- 04-constantes-com-const/constantes.js
+++ 04-constantes-com-const/constantes.js
@@ -6,3 +6,10 @@
 // Case-sensitive
 // Não podemos modificar o valor de uma constante
 // NÃO UTILIZE VAR, UTILIZE CONST.
+
+const primeironumero = 10;
+const segundonumero = 20;
+const resultado = primeironumero * segundonumero
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+console.log(resultadoTriplicado);
