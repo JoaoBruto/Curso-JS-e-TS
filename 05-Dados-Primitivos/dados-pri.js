fdfd51435b37c3334e41a6355157140811702a11
João Rodrigo <joaofreire185710@gmail.com>
2025-08-15 16:37:42 -0300

    📖 Operators

---
+++ 05-Dados-Primitivos/dados-pri.js
@@ -0,0 +1,9 @@
+const nome = 'João'; // String
+
+const numb = 10 // Number
+const numb2  =10.65 // Number
+
+let nomeAluno; // Undefined
+
+const boolean = true; // Booleano
+const boolean2 = false; // booleano
