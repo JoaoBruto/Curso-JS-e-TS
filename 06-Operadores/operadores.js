7aac08d2707dd5fbda68bead7cfd2cd9c0cdbb58
João Rodrigo <joaofreire185710@gmail.com>
2025-08-18 16:41:42 -0300

    📖 Operadores de atribuição, aritméticos e incremento

---
+++ 06-Operadores/operadores.js
@@ -0,0 +1,22 @@
+// Aritméticos: + - / *
+// + = Adição/Concatenação
+// - = Subtração
+// / = Divisão
+// * = Multiplicação
+// ** = Potenciação
+// % = Resto da divisão
+
+const num1 = 15
+const num2 = 5
+console.log(num1 + num2)
+
+//Incremento = ++
+//Decremento = --
+let contador = 10;
+contador--;
+console.log(contador)
+
+// Atribuição
+let numero = 2;
+numero += 2; // Mesma coisa que numero = numero + 2
+console.log(numero)
