f1d3dad8826cb87cc283c643cebd9f88c7514cd7
João Rodrigo <joaofreire185710@gmail.com>
2025-09-02 21:02:17 -0300

    ⚠ Alert,, Prompt e Confirm

---
+++ 07-Alert-confirm-prompt/tests.js
@@ -0,0 +1,12 @@
+// alert('Hello World'); // Alerta na tela do usuário
+// confirm('Deseja realmente apagar?'); // Pede pro usuário confirmar algo, por exemplo: "Deseja realmente apagar?"
+// prompt('Digite o seu nome:'); // Dá uma caixa de input pro usuuário
+// const confirma = confirm('Realmente deseja apagar?');
+// console.log(confirma);
+
+let num1 = 500; // string
+num1 = parseFloat(num1) // Transformar em number/float
+let num2 = 10;
+num2 = Number(num2)
+
+console.log(num1 + num2);
