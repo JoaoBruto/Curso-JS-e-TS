8f314c8c67d14bf2bacd4d20cd40c9b72700a738
João Rodrigo <joaofreire185710@gmail.com>
2025-09-03 14:23:43 -0300

    📝 Exercicio: Variáveis

---
+++ 08- Exercicio-variáveis/script.js
@@ -0,0 +1,12 @@
+let VarA = "A"; // B
+let VarB = "B"; // C
+let VarC = "C"; // A
+
+const VarAtemp = VarB; // Transforma "VarA" em B
+const VarBtemp = VarC; // Transforma "VarB" em C
+const VarCtemp = VarA; // Transforma "VarC" em A
+
+console.log(VarAtemp, VarBtemp, VarCtemp) // Imprime os resultados
+
+
+
