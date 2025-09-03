455882328f576937b59f368cdb6115add0b3763b
João Rodrigo <joaofreire185710@gmail.com>
2025-09-02 21:07:35 -0300

    ⚠ Exercicio: Alert, Prompt e Confirm

---
+++ 07-Alert-confirm-prompt/Exercicio/script.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Digite o primeiro número da soma"); // Input pro usuário inserir o primeiro número
+num1 = parseFloat(num1); // Transformando uma string em number/float
+
+let num2 = prompt("Digite o segundo número da soma"); // Input pro usuário inserir o sggundo número
+num2 = Number(num2); // Transformando uma string em number/float
+
+console.log(num1 + num2); // Resultado da soma
