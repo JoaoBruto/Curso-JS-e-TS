eebc24d35ea9ff1a481a73883d6e77f59072bce6
Joao Rodrigo <joaofreire185710@gmail.com>
2025-10-20 22:59:22 -0300

    ⚠️ Mais-Sobre-Numbers

---
+++ 10-MaisSobreNumbers/script.js
@@ -0,0 +1,13 @@
+let num1 = 1010; // Number
+let num2 = 2; // Number
+
+console.log(num1.toString() + num2); // Ele só se torna uma string nessa linha, o valor original de num1 continua sendo number
+console.log(typeof num1); // number
+
+// Transformar em binário
+console.log(num1.toString(2));
+
+//Arrendondar casas decimais
+
+let valor_pi = 3.14159265359;
+console.log(valor_pi.toFixed(2)); // 3.14
