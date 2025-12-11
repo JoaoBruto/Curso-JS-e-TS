cb72c5d849369952a679f9d303a89a70056fcd4c
Joao Rodrigo <joaofreire185710@gmail.com>
2025-12-10 23:51:43 -0300

    ✨ | Aula sobre Math | Arredondamento

---
+++ 11-Objeto-Math/script.js
@@ -0,0 +1,4 @@
+let num1 = 9.674;
+// let num2 = Math.floor(num1); Arredonda para baixo. (Inteiro mais próximo)
+let num2 = Math.ceil(num1); // Arredonda para a cima. (Inteiro mais próximo. 10 no caso)
+console.log(num2); 
