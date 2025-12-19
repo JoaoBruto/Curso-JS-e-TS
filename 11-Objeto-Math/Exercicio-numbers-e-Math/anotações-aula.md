c2f206a5c917ef7ab4276a038035a6c516b73387
Joao Rodrigo <joaofreire185710@gmail.com>
2025-12-18 22:53:54 -0300

    ✨ | Aula sobre Math e Number- Exercicios => Revisado com o curso

---
+++ 11-Objeto-Math/Exercicio-numbers-e-Math/anotações-aula.md
@@ -0,0 +1,7 @@
+Na parte do "h1", onde eu usei "span" ao invés de "div", eu usei "span", porque ele é mais adequado por se tratar de um elemento in-line e também melhor adaptado a pequenos textos e etc. Enquanto, "div" é melhor para elementos e grupos inteiros.
+
+Pegar um elemento do HTML e modificar ele usando o JavaScript:
+const "" = document.getElementById();
+
+Para aparecer o elemento na tela do usuário, deve-se usar essa propriedade:
+"propriedade que a gente pegou o elemento do HTML pelo ID".inner.html = "propriedade quer que aconteça"
--- 11-Objeto-Math/Exercicio-numbers-e-Math/exercicio.js
+++ 11-Objeto-Math/Exercicio-numbers-e-Math/exercicio.js
@@ -1,31 +1,41 @@
 // Inputar o número
 let num1 = parseFloat(prompt("Digite um número: "));
+const numeroSelecionado = document.getElementById('numero-selecionado')
+const texto = document.getElementById('texto')
 
-
+numeroSelecionado.innerHTML = num1; // Modificando o body do HTML pelo JS (arquivo separado)
 
 // Usando propriedades do Math
 
 // Raiz quadrada
 const raiz_quadrada = num1 ** 0.5; // Raiz quadrada usando propriedade.
-const raiz_quadrada_resultado = alert(`A raiz quadrada de ${num1} é ${raiz_quadrada}`)
+const Raiz_quadrada = document.getElementById('raiz-quadrada');
+Raiz_quadrada.innerHTML = raiz_quadrada;
 
 // Saber se é inteiro
 const inteiro = Number.isInteger(num1); // Verifica se o número é inteiro.
-const inteiro_resultado = alert(`O número ${num1} é inteiro? ${inteiro}`);
+const Numero_selecionado_pelo_usuário = document.getElementById("numero-selecionado-para-saber-se-é-inteiro"); // Pega o número inserido pelo usuário
+const Resultado_inteiro = document.getElementById("resultado-int"); // Pega do HTML o id para modificar
+Numero_selecionado_pelo_usuário.innerHTML = num1; // Muda o número no HTML, pelo número selecionado pelo usuário
+Resultado_inteiro.innerHTML = inteiro; // Imprime o resultado
 
 // Saber se é NaN
 const nan = Number.isNaN(num1); // Verifica se o número é NaN.
-const nan_resultado = alert(`O número ${num1} é NaN? ${nan}`);
+const Resultado_NaN = document.getElementById("resultado-NaN");
+Resultado_NaN.innerHTML = nan;
 
 // Arredondar para baixo e cima
 const arrendondar_baixo = Math.floor(num1); // Arredonda para baixo.
-const arrendondar_baixo_resultado = alert(`O número ${num1} arredondado para baixo é ${arrendondar_baixo}`);
+const Resultado_Arredondamento = document.getElementById("Resultado-arredondamento-baixo");
+Resultado_Arredondamento.innerHTML = arrendondar_baixo;
 
 const arrendondar_cima = Math.ceil(num1); // Arredonda para cima.
-const arrendondar_cima_resultado = alert(`O número ${num1} arredondado para cima é ${arrendondar_cima}`);
+const Resultado_Arredondamento_cima = document.getElementById("Resultado-arredondamento-alto");
+Resultado_Arredondamento_cima.innerHTML = arrendondar_cima
 
 
 // Arrendondar duas casas decimais
 const arredondar_casasdecimais = num1.toFixed(2); // Arredonda para duas casas decimais.    
-const arredondar_casasdecimais_resultado = alert(`O arredondamento de ${num1} para duas casas é ${arredondar_casasdecimais}`)
+const Resultado_casasdecimais = document.getElementById("Resultado-duas-casas-decimais")
+Resultado_casasdecimais.innerHTML = arredondar_casasdecimais
 
