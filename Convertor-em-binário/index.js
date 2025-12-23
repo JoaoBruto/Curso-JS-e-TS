b5ceb2296fa70e5a910bcf9caaf4d18c1aa0bd3b
Joao Rodrigo <joaofreire185710@gmail.com>
2025-12-23 00:20:30 -0300

    📖 | Implementação de erro, caso o usuário digite alguma letra (NaN)

--- Convertor-em-binário/index.js
+++ Convertor-em-binário/index.js
@@ -5,3 +5,9 @@ Numero_escolhido.innerHTML = escolher_numero;
 let numero_binario = escolher_numero.toString(2);
 const Resultado = document.getElementById("numero_binario")
 Resultado.innerHTML = numero_binario;
+
+if (isNaN(escolher_numero)) {
+    alert("Por favor, insira um número.");
+    Numero_escolhido.innerHTML = "Não é um número válido";
+    Resultado.innerHTML = "Não é um número válido";
+}
