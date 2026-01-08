46c929e17fe6fad2150d870121af942ed4ad1d8a
Joao Rodrigo <joaofreire185710@gmail.com>
2026-01-07 21:18:02 -0300

    ✨ feat: Second method of creation functions

--- 13-Function/script.js
+++ 13-Function/script.js
@@ -1,6 +1,5 @@
-function soma(x, y) {
-    const resultado = x + y;
-    return resultado;
-}
+const raiz = function(numero) {
+    return numero ** 0.5
+};
 
-console.log(soma(2, 2))
+console.log(raiz(9))
